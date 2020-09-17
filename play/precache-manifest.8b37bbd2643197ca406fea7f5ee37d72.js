50f47804876073ff7eb039409e17e1547d9dc4fd
Artur Szott <artur.szott@gmail.com>
2020-09-17 09:38:17 +0200

    Add playable static version of the game

---
+++ play/precache-manifest.8b37bbd2643197ca406fea7f5ee37d72.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2908be1064f320d72de5e037d91452e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "301bcb2bf63df58d1c11",
+    "url": "./static/css/main.431f10a2.chunk.css"
+  },
+  {
+    "revision": "19369318be6cd6a7ef56",
+    "url": "./static/js/2.5472b026.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.5472b026.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "301bcb2bf63df58d1c11",
+    "url": "./static/js/main.0b4c7592.chunk.js"
+  },
+  {
+    "revision": "dcec8fe9cd8016444315",
+    "url": "./static/js/runtime-main.17b94824.js"
+  }
+]);
